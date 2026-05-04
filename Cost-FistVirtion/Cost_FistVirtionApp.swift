f12139f1746ec2af02006319f781b37f23f2a0b5
Lluvia <Yyuxuan.Cheng@student.uts.edu.au>
2026-05-04 16:42:17 +1000

    Initial Commit

---
+++ Cost-FistVirtion/Cost_FistVirtionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cost_FistVirtionApp.swift
+//  Cost-FistVirtion
+//
+//  Created by Yuxuan Cheng on 4/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct Cost_FistVirtionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
